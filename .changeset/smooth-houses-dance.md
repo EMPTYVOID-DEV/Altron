607caf704a89e8a8760788a1089b901386252cc0
Shinji13 <keskasaymen8@gmail.com>
2024-01-31 01:11:01 +0100

    fixing the package exports

---
+++ .changeset/smooth-houses-dance.md
@@ -0,0 +1,5 @@
+---
+'@altron/altron': patch
+---
+
+fixing the package exports
--- src/lib/index.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-
-export { default as Altron } from './components/core/main.svelte';
